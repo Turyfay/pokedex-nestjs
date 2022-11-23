e69141806248342a5609a2e5e84ab0e8b9d6614a
David Enamorado <david8enaceituno@gmail.com>
2022-11-22 21:50:59 -0600

    add: conexion nest con mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      expandVariables: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot(process.env.DB_URL, {}),
     PokemonModule,
   ],
   controllers: [],
