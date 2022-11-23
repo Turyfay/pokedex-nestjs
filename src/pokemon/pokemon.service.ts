1b161d38830f1a5dd366fdd82fd103b4e8ecfc66
David Enamorado <david8enaceituno@gmail.com>
2022-11-22 23:49:05 -0600

    add: responder los codigos de error del db

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
@@ -11,8 +15,19 @@ export class PokemonService {
     @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
   ) {}
   async create(createPokemonDto: CreatePokemonDto) {
-    const pokemon = this.pokemonModel.create(createPokemonDto);
-    return pokemon;
+    try {
+      const pokemon = this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+      throw new InternalServerErrorException(
+        'Error server, contacte al administrador',
+      );
+    }
   }
 
   findAll() {
