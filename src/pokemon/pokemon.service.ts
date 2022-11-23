7dd9aaa09b71146b9c50882dce3f7ac217c707ae
David Enamorado <david8enaceituno@gmail.com>
2022-11-23 01:18:10 -0600

    add: validar y eliminar en una consulta

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -74,7 +74,13 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    /* const pokemon = await this.findOne(id);
+    await pokemon.deleteOne(); */
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id ${id} not found`);
+
+    return;
   }
 }
